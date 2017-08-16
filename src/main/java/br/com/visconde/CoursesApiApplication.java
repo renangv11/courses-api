f2e0f3e2e7d233576077364b0ff58ae6cd92f79a
Visconde <renan.g.visconde@accenture.com>
2017-08-16 20:02:14 -0300

    first commit

---
+++ src/main/java/br/com/visconde/CoursesApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.visconde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesApiApplication.class, args);
+	}
+}
---
+++ src/main/java/br/com/visconde/topic/TopicService.java
@@ -0,0 +1,38 @@
+package br.com.visconde.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	@Autowired
+	private TopicRepository topicRepository;
+
+	public List<Topic> getAllTopics() {
+		List<Topic> topics = new ArrayList<Topic>();
+		topicRepository.findAll().forEach(topics::add);
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topicRepository.findOne(id);
+	}
+
+	public void addTopic(Topic topic) {
+		topicRepository.save(topic);
+	}
+
+	public void updateTopic(Topic topic) {
+		topicRepository.save(topic);
+	}
+
+	public void deleteTopic(String id) {
+		topicRepository.delete(id);
+	}
+
+}
